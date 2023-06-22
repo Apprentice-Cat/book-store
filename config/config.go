4f0239204735f95e649cdd4a4b9ed6744dac3dc0
dinhuy9595 <dinhuy9595@gmail.com>
2023-06-22 23:54:17 +0700

    add config

---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	Database struct {
+		User                 string
+		Password             string
+		Net                  string
+		Addr                 string
+		Port                 string
+		DBName               string
+		AllowNativePasswords bool
+		Params               struct {
+			ParseTime string
+		}
+	}
+	Server struct {
+		Address string
+	}
+}
+
+var C config
+
+func ReadConfig() {
+	Config := &C
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(filepath.Join("$GOPATH", "src", "config"))
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		log.Fatalln(err)
+	}
+
+	if err := viper.Unmarshal(&Config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
